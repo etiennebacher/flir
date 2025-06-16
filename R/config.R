808b4cb374d73fc9eda77c53878c81729a95f680
etiennebacher <etienne.bacher@protonmail.com>
2025-06-16 22:36:31 +0200

    use cli::cli_abort()

--- R/config.R
+++ R/config.R
@@ -24,7 +24,7 @@ check_config <- function(path) {
   nms <- names(yaml::read_yaml(config_file, readLines.warn = FALSE))
   nms_unexpected <- setdiff(nms, c("keep", "exclude", "from-package"))
   if (length(nms_unexpected) > 0) {
-    stop(sprintf(
+    cli::cli_abort(sprintf(
       "Unknown field in `flir/config.yml`: %s",
       toString(nms_unexpected)
     ))
@@ -41,14 +41,20 @@ get_linters_from_config <- function(path) {
     "from-package"
   ]]
   if (length(linters) == 0 && length(from_package) == 0) {
-    stop("`", config_file, "` exists but doesn't contain any rule.")
+    cli::cli_abort(paste0(
+      "`",
+      config_file,
+      "` exists but doesn't contain any rule."
+    ))
   }
   if (anyDuplicated(linters) > 0) {
-    stop(
-      "In `",
-      config_file,
-      "`, the following linters are duplicated: ",
-      toString(linters[duplicated(linters)])
+    cli::cli_abort(
+      paste0(
+        "In `",
+        config_file,
+        "`, the following linters are duplicated: ",
+        toString(linters[duplicated(linters)])
+      )
     )
   }
   linters
@@ -65,12 +71,10 @@ get_excluded_linters_from_config <- function(path) {
     return(NULL)
   }
   if (anyDuplicated(linters) > 0) {
-    stop(
-      "In `",
-      config_file,
-      "`, the following excluded linters are duplicated: ",
+    cli::cli_abort(c(
+      "In {.path {config_file}}, the following excluded linters are duplicated: ",
       toString(linters[duplicated(linters)])
-    )
+    ))
   }
   linters
 }
@@ -86,12 +90,10 @@ get_external_linters_from_config <- function(path) {
     return(NULL)
   }
   if (anyDuplicated(pkgs) > 0) {
-    stop(
-      "In `",
-      config_file,
-      "`, the following packages are duplicated: ",
+    cli::cli_abort(paste0(
+      "In {.path {config_file}}, the following packages are duplicated: ",
       toString(pkgs[duplicated(pkgs)])
-    )
+    ))
   }
 
   installed <- pkgs[pkgs == basename(pkgs)]
