470dc510996a1f5a30ac36863571ee507e455e39
etiennebacher <etienne.bacher@protonmail.com>
2025-06-16 23:05:53 +0200

    fix

--- tests/testthat/_snaps/setup_flint_gha.md
+++
@@ -1,8 +0,0 @@
-# setup_flir_gha basic use works
-
-    Code
-      setup_flir_gha()
-    Condition
-      Error:
-      ! [EEXIST] Failed to copy '/home/etienne/Desktop/Git/flir/inst/gha/flir.yaml' to './.github/workflows/flir.yaml': file already exists
-
--- tests/testthat/test-setup_flir_gha.R
+++ tests/testthat/test-setup_flir_gha.R
@@ -3,10 +3,8 @@ test_that("setup_flir_gha basic use works", {
   expect_no_error(suppressMessages(setup_flir_gha()))
   expect_true(file.exists(".github/workflows/flir.yaml"))
 
-  expect_snapshot(
-    setup_flir_gha(),
-    error = TRUE
-  )
-
+  # Do not use snapshot here since error message includes path to installed
+  # "flir"
+  expect_error(setup_flir_gha(), "file already exists")
   expect_no_error(setup_flir_gha(overwrite = TRUE))
 })
