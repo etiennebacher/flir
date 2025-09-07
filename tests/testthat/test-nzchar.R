ccafdaf6a30c2ec00692384d91318c42dc8ff31b
Trevor L. Davis <trevor.l.davis@gmail.com>
2025-09-07 10:08:37 -0700

    tests: Use `expect_no_lint()`

--- tests/testthat/test-nzchar.R
+++ tests/testthat/test-nzchar.R
@@ -1,23 +1,23 @@
 test_that("nzchar_linter skips allowed usages", {
   linter <- nzchar_linter()
 
-  expect_lint("if (any(nzchar(x))) TRUE", NULL, linter)
+  expect_no_lint("if (any(nzchar(x))) TRUE", linter)
 
-  expect_lint("letters == 'a'", NULL, linter)
+  expect_no_lint("letters == 'a'", linter)
 
-  expect_lint("which(nchar(x) == 4)", NULL, linter)
-  expect_lint("which(nchar(x) != 2)", NULL, linter)
+  expect_no_lint("which(nchar(x) == 4)", linter)
+  expect_no_lint("which(nchar(x) != 2)", linter)
 
   # following tests not from {lintr}
-  expect_lint('nchar(x, type = "width") > 0', NULL, linter)
-  expect_lint('nchar(x, type = "width") != 0', NULL, linter)
-  expect_lint('nchar(x, type = "width") <= 0', NULL, linter)
-  expect_lint('nchar(x, type = "width") == 0', NULL, linter)
-  expect_lint('nchar(x, type = "width") >= 0', NULL, linter)
-  expect_lint('nchar(x, type = "width") < 0', NULL, linter)
-  expect_lint('nchar(x) >= 02', NULL, linter)
-  expect_lint('nchar(x) >= 02L', NULL, linter)
-  expect_lint('nchar(x) == 0.5', NULL, linter)
+  expect_no_lint('nchar(x, type = "width") > 0', linter)
+  expect_no_lint('nchar(x, type = "width") != 0', linter)
+  expect_no_lint('nchar(x, type = "width") <= 0', linter)
+  expect_no_lint('nchar(x, type = "width") == 0', linter)
+  expect_no_lint('nchar(x, type = "width") >= 0', linter)
+  expect_no_lint('nchar(x, type = "width") < 0', linter)
+  expect_no_lint('nchar(x) >= 02', linter)
+  expect_no_lint('nchar(x) >= 02L', linter)
+  expect_no_lint('nchar(x) == 0.5', linter)
 })
 
 test_that("nzchar_linter skips as appropriate for other nchar args", {
@@ -26,19 +26,19 @@ test_that("nzchar_linter skips as appropriate for other nchar args", {
   # using type="width" can lead to 0-width strings that are counted as
   #   nzchar, c.f. nchar("\u200b", type="width"), so don't lint this.
   # type="bytes" should be >= the value for the default (type="chars")
-  expect_lint("nchar(x, type='width') == 0L", NULL, linter)
+  expect_no_lint("nchar(x, type='width') == 0L", linter)
 
   # nchar(x) with invalid multibyte strings -->
   #   error, while nzchar(x) returns TRUE for those entries.
   # nchar(x, allowNA=TRUE) with invalid multibyte strings -->
   #   NA in each element with an invalid entry, while nzchar returns TRUE.
-  expect_lint("nchar(x, allowNA=TRUE) == 0L", NULL, linter)
+  expect_no_lint("nchar(x, allowNA=TRUE) == 0L", linter)
 
   skip("`nzchar_linter()` currently only handles `nchar(x)` with single arg")
   # nzchar also has keepNA argument so a drop-in switch is easy
   expect_lint(
     "nchar(x, keepNA=TRUE) == 0",
-    rex::rex("Use !nzchar(x) instead of nchar(x) == 0"),
+    "Use !nzchar(x) instead of nchar(x) == 0",
     linter
   )
 })
@@ -130,13 +130,13 @@ test_that("nzchar_linter skips comparison to '' in if/while statements", {
   skip(
     "`nzchar_linter()` does not skip comparison to '' in if/while statements"
   )
-  expect_lint('if (x == "") TRUE', NULL, linter)
-  expect_lint('while (x == "") TRUE', NULL, linter)
+  expect_no_lint('if (x == "") TRUE', linter)
+  expect_no_lint('while (x == "") TRUE', linter)
 
   # nested versions, a la nesting issues with vector_logic_linter
-  expect_lint('if (TRUE || (x == "" && FALSE)) TRUE', NULL, linter)
-  expect_lint('if (TRUE && x == "" && FALSE) TRUE', NULL, linter)
-  expect_lint('foo(if (x == "") y else z)', NULL, linter)
+  expect_no_lint('if (TRUE || (x == "" && FALSE)) TRUE', linter)
+  expect_no_lint('if (TRUE && x == "" && FALSE) TRUE', linter)
+  expect_no_lint('foo(if (x == "") y else z)', linter)
 })
 
 test_that("multiple lints are generated correctly", {
