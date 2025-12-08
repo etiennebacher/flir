86714f5a2d8c46e15d795821fed79a8168d2562e
etiennebacher <etienne.bacher@protonmail.com>
2025-12-08 22:21:02 +0100

    init

--- tests/testthat/test-custom_linters.R
+++ tests/testthat/test-custom_linters.R
@@ -1,5 +1,65 @@
-test_that("users can define custom linters", {
+test_that("users can define custom linters in a package", {
   create_local_package()
+  setup_flir()
+  fs::dir_create("flir/rules/custom")
+  fs::dir_create("dir1/dir2")
+
+  cat(
+    "id: foobar
+language: r
+severity: warning
+rule:
+  pattern: unique(length($VAR))
+fix: length(unique(~~VAR~~))
+message: Most likely an error
+",
+    file = "flir/rules/custom/AAAAAAAAA.yml"
+  )
+
+  cat("x <- function() { \nunique(length(x))\n}", file = "dir1/dir2/foo.R")
+
+  config <- yaml::read_yaml("flir/config.yml")
+  config$keep <- c(config$keep, "AAAAAAAAA")
+  yaml::write_yaml(config, "flir/config.yml")
+  withr::with_envvar(
+    new = c("TESTTHAT" = FALSE, "GITHUB_ACTIONS" = FALSE),
+    {
+      expect_equal(nrow(lint(use_cache = FALSE, verbose = FALSE)), 1)
+      # Passing a specific file path works with custom linters, #54
+      expect_equal(
+        nrow(lint("dir1/dir2/foo.R", use_cache = FALSE, verbose = FALSE)),
+        1
+      )
+
+      expect_equal(
+        nrow(
+          lint(
+            use_cache = FALSE,
+            linters = list_linters(path = "."),
+            verbose = FALSE
+          )
+        ),
+        0
+      )
+      fix(path = ".", verbose = FALSE)
+    }
+  )
+  expect_true(
+    any(
+      grepl(
+        "length(unique(x))",
+        readLines("dir1/dir2/foo.R", warn = FALSE),
+        fixed = TRUE
+      )
+    )
+  )
+})
+
+# The important part here is that this is a project that is not a package and
+# is not an RStudio project, meaning that rprojroot doesn't find the root of
+# the path, #118.
+test_that("users can define custom linters in a project", {
+  create_local_project()
   setup_flir()
   fs::dir_create("flir/rules/custom")
   fs::dir_create("dir1/dir2")
